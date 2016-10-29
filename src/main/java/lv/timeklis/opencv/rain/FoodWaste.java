23bef3c88871bd7cd160bc16a5a6f3351427a16e
Martins Balodis <martins256@gmail.com>
2016-10-29 12:31:26 +0300

    find circles

--- src/main/java/lv/timeklis/opencv/rain/FoodWaste.java
+++ src/main/java/lv/timeklis/opencv/rain/FoodWaste.java
@@ -18,6 +18,45 @@ public class FoodWaste {
     public FoodWaste(String img) {
 
         Mat src = Highgui.imread(img, Highgui.CV_LOAD_IMAGE_COLOR);
-        Preview.preview(src, "input");
+//        Preview.preview(src, "input");
+
+        Mat gray = new Mat(src.size(), CvType.CV_8UC1);
+        Imgproc.cvtColor(src, gray, Imgproc.COLOR_RGB2GRAY);
+
+        findCircles(src, gray);
+
+//        Preview.preview(gray, "gray");
+
+    }
+
+    private void findCircles(Mat src, Mat gray) {
+
+        Mat circles = new Mat();
+
+        int iCannyUpperThreshold = 100;
+        int iMinRadius = 50;
+        int iMaxRadius = 1000;
+        int iAccumulator = 100;
+
+        Imgproc.HoughCircles(gray, circles, Imgproc.CV_HOUGH_GRADIENT,
+                1.5, 70, iCannyUpperThreshold, iAccumulator,
+                iMinRadius, iMaxRadius);
+
+        for (int x = 0; x < circles.cols(); x++) {
+            double vCircle[] = circles.get(0, x);
+
+            Mat copy = src.clone();
+
+            Point center = new Point(Math.round(vCircle[0]), Math.round(vCircle[1]));
+            int radius = (int) Math.round(vCircle[2]);
+            // draw the circle center
+            Core.circle(copy, center, 3, new Scalar(0, 255, 0), -1, 8, 0);
+            // draw the circle outline
+            Core.circle(copy, center, radius, new Scalar(0, 0, 255), 3, 8, 0);
+
+            Preview.preview(copy, "circled");
+        }
+
+
     }
 }
